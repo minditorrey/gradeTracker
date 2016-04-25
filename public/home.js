74a7506a1044ba764e8b48a40019cd16d44dc402
Mindi Torrey <minditorrey@me.com>
2016-04-25 13:06:29 -0700

    update table and modal

--- public/home.js
+++ public/home.js
@@ -15,7 +15,20 @@ function createNewGrade(e) {
 		name: $('#name').val(),
 		score: $('#score').val(),
 		totalPoints: $('#totalPoints').val(),
-		letterGrade: $('#letterGrade').val(),
+		letterGrade: function(score){
+			if(score >90) {
+				return "A";
+			} else if(score > 80 < 90) {
+				return "B";
+			} else if(score > 70 < 80) {
+				return "C";
+			} else if(score > 60 < 70) {
+				return "D";
+			} else {
+				return "F";
+			}
+		},
+
 		cumulativeGrade: $('#cumulativeGrade').val()
 	};
 
